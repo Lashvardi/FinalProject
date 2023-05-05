151d04a3d5826b301892225d981701eb41038d47
Lashvardi <lashakakashvili114@gmail.com>
2023-05-05 18:57:32 +0400

    ASF

--- src/app/components/filtered/filtered.component.ts
+++ src/app/components/filtered/filtered.component.ts
@@ -5,60 +5,72 @@ import { GetCarService } from 'src/app/services/get-car.service';
 @Component({
   selector: 'app-filtered',
   templateUrl: './filtered.component.html',
-  styleUrls: ['./filtered.component.scss']
+  styleUrls: ['./filtered.component.scss'],
 })
 export class FilteredComponent {
-
   cars: Car[] = [];
   public cityName: string = '';
+  capacities = [1, 2, 3, 4, 5, 6, 7, 8];
   public yearRange: string = '';
   public cities: string[] = [];
-  public capacity: number = 0;
-  constructor(public getCar: GetCarService){
-    this.getCar.getCities()
-    .subscribe(cities => {
-      this.cities = cities;
-    }, error => {
-      console.error(error);
-    });
+  public selectedCapacity: number = 0;
+  constructor(public getCar: GetCarService) {
+    this.getCar.getCities().subscribe(
+      (cities) => {
+        this.cities = cities;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
 
-    
-    this.getCar.getCarsByYear(1995, 2020)
-  .subscribe(cars => {
-      this.cars = cars;
-  }, error => {
-    console.error(error);
-  });
-
+    this.getCar.getCarsByYear(1995, 2020).subscribe(
+      (cars) => {
+        this.cars = cars;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
   }
 
   getCarsByYearAndCity() {
-    if (!this.cityName && !this.yearRange && !this.capacity) {
-      this.getCar.GetCars()
-        .subscribe(cars => this.cars = cars, error => console.error(error));
-    } else if (this.cityName && !this.yearRange && !this.capacity) {
-      this.getCar.getCarsByCity(this.cityName )
-        .subscribe(cars => this.cars = cars, error => console.error(error));
-    } else if (!this.cityName && this.yearRange ) {
+    if (!this.cityName && !this.yearRange) {
+      this.getCar.GetCars().subscribe(
+        (cars) => (this.cars = cars),
+        (error) => console.error(error)
+      );
+    } else if (this.cityName && !this.yearRange) {
+      this.getCar.getCarsByCity(this.cityName).subscribe(
+        (cars) => (this.cars = cars),
+        (error) => console.error(error)
+      );
+    } else if (!this.cityName && this.yearRange) {
       let startYear = 0;
       let endYear = 0;
       if (this.yearRange) {
-        [startYear, endYear] = this.yearRange.split('-').map(y => parseInt(y));
+        [startYear, endYear] = this.yearRange
+          .split('-')
+          .map((y) => parseInt(y));
       }
-      this.getCar.getCarsByYear(startYear, endYear)
-        .subscribe(cars => this.cars = cars, error => console.error(error));
+      this.getCar.getCarsByYear(startYear, endYear).subscribe(
+        (cars) => (this.cars = cars),
+        (error) => console.error(error)
+      );
     } else {
       let startYear = 0;
       let endYear = 0;
       if (this.yearRange) {
-        [startYear, endYear] = this.yearRange.split('-').map(y => parseInt(y));
+        [startYear, endYear] = this.yearRange
+          .split('-')
+          .map((y) => parseInt(y));
       }
-      this.getCar.getCarsByCityAndYear(this.cityName, startYear, endYear)
-        .subscribe(cars => this.cars = cars, error => console.error(error));
+      this.getCar
+        .getCarsByCityAndYear(this.cityName, startYear, endYear)
+        .subscribe(
+          (cars) => (this.cars = cars),
+          (error) => console.error(error)
+        );
     }
   }
-  
-
-
-
 }
--- src/app/services/get-car.service.ts
+++ src/app/services/get-car.service.ts
@@ -4,46 +4,44 @@ import { ServiceUrlBuilder } from 'src/ServiceUrlBuilder';
 import { Car } from '../models/car';
 import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GetCarService {
+  private static RESOURCE_URI: string = 'Car';
+  private static Popular_URi: string = 'Car/popular';
+  private static Favorite_URI: string = 'Users';
 
-  private static RESOURCE_URI: string = "Car";
-  private static Popular_URi: string = "Car/popular";
-  private static Favorite_URI: string = "Users";
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   public GetCars() {
-    return this.http.get<Car[]>(ServiceUrlBuilder.buildUrl(GetCarService.RESOURCE_URI));
+    return this.http.get<Car[]>(
+      ServiceUrlBuilder.buildUrl(GetCarService.RESOURCE_URI)
+    );
   }
 
   public GetCarById(id: number) {
-    return this.http.get<Car>(ServiceUrlBuilder.buildUrl(`${GetCarService.RESOURCE_URI}/${id}`));
+    return this.http.get<Car>(
+      ServiceUrlBuilder.buildUrl(`${GetCarService.RESOURCE_URI}/${id}`)
+    );
   }
 
   public getPopularCars() {
-    return this.http.get<Car[]>(ServiceUrlBuilder.buildUrl(GetCarService.Popular_URi));
+    return this.http.get<Car[]>(
+      ServiceUrlBuilder.buildUrl(GetCarService.Popular_URi)
+    );
   }
 
   public GetFavoriteCars() {
     const phoneNumber = localStorage.getItem('PhoneNumber') || '';
-    return this.http.get<Car[]>(ServiceUrlBuilder.buildUrl(`${GetCarService.Favorite_URI}/${phoneNumber}/favorite-cars`));
+    return this.http.get<Car[]>(
+      ServiceUrlBuilder.buildUrl(
+        `${GetCarService.Favorite_URI}/${phoneNumber}/favorite-cars`
+      )
+    );
   }
-  
-  public getCapacityFilteredCars(capacity: number): Observable<Car[]> {
-    return this.http.get<Car[]>(ServiceUrlBuilder.buildUrl(`${GetCarService.RESOURCE_URI}/byCapacity?capacity=${capacity}`));
-  }
-  
-
 
   getCarsByYear(startYear: number, endYear: number) {
     const url = `Car/byYear?startYear=${startYear}&endYear=${endYear}`;
-    return this.http.get<Car[]>(ServiceUrlBuilder.buildUrl(url));
-  }
-
-  getCarsByYearPaginated(startYear: number, endYear: number, pageIndex: number, pageSize: number) {
-    const url = `Car/byYear?startYear=${startYear}&endYear=${endYear}&pageIndex=${pageIndex}&pageSize=${pageSize}`;
     return this.http.get<Car[]>(ServiceUrlBuilder.buildUrl(url));
   }
 
@@ -52,7 +50,11 @@ export class GetCarService {
     return this.http.get<Car[]>(ServiceUrlBuilder.buildUrl(url));
   }
 
-  public getCarsByCityAndYear(cityName: string, startYear: number, endYear: number) {
+  public getCarsByCityAndYear(
+    cityName: string,
+    startYear: number,
+    endYear: number
+  ) {
     const url = `${GetCarService.RESOURCE_URI}/byCityAndYear?cityName=${cityName}&startYear=${startYear}&endYear=${endYear}`;
     return this.http.get<Car[]>(ServiceUrlBuilder.buildUrl(url));
   }
