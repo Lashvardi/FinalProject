1bf12444dbe46f53b9b982412c903ec790c1295d
Lashvardi <lashakakashvili114@gmail.com>
2023-05-10 13:01:45 +0400

    Added Sample Page For Purchasing The car

--- src/app/components/car-rent-detail/car-rent-detail.component.ts
+++ src/app/components/car-rent-detail/car-rent-detail.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Car } from 'src/app/models/car';
 import { GetCarService } from 'src/app/services/get-car.service';
+import { PurchaseCarService } from 'src/app/services/purchase-car.service';
 
 @Component({
   selector: 'app-car-rent-detail',
@@ -10,10 +11,12 @@ import { GetCarService } from 'src/app/services/get-car.service';
 })
 export class CarRentDetailComponent {
   car: Car | undefined;
-
+  multiplier: number = 1;
+  PhoneNumber: string = localStorage.getItem("PhoneNumber") || "";
   constructor(
     private route: ActivatedRoute,
-    private carService: GetCarService
+    private carService: GetCarService,
+    private purchase: PurchaseCarService
   ) {}
 
   ngOnInit() {
@@ -22,6 +25,20 @@ export class CarRentDetailComponent {
       const carId = +carIdString;
       this.getCarDetails(carId);
     }
+  }
+
+  BuyCar(phoneNumber: string, carId: number, multiplier: number) {
+    phoneNumber = this.PhoneNumber;
+    carId = this.car?.id || 0;
+     multiplier = this.multiplier;
+    this.purchase.purchaseCar(phoneNumber, carId, multiplier).subscribe(
+      (response) => {
+        console.log('Car purchased successfully:', response);
+      },
+      (error) => {
+        console.error('Error occurred while purchasing car:', error);
+      }
+    );
   }
 
   getCarDetails(carId: number) {
--- src/app/services/purchase-car.service.ts
+++ src/app/services/purchase-car.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
 import { Car } from '../models/car';
 import { ServiceUrlBuilder } from 'src/ServiceUrlBuilder';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class PurchaseCarService {
-  public static RESOURCE_URL = "/Purchase/Purchase"
+  public static RESOURCE_URL = "Purchase/Purchase"
   constructor(public http: HttpClient) { }
 
   public purchaseCar(PhoneNumber: string, CarId: number, Multiplier: number) {
-    return this.http.post<ServiceUrlBuilder>(PurchaseCarService.RESOURCE_URL, { PhoneNumber, CarId , Multiplier});
-  }
+    return this.http.post(ServiceUrlBuilder.buildRootUrl(`${PurchaseCarService.RESOURCE_URL}`), null, {
+      params: new HttpParams()
+        .set('phoneNumber', PhoneNumber)
+        .set('carId', CarId.toString())
+        .set('multiplier', Multiplier.toString())
+    });
+      }
 
   public getPurchasedCars(PhoneNumber: string){
     return this.http.get<Car[]>(ServiceUrlBuilder.buildUrl(`${PurchaseCarService.RESOURCE_URL}/${PhoneNumber}`));
